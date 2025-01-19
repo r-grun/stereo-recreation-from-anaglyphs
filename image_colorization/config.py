498281ab7e6a1312976af12278b8217f13f53897
Robin <44874951+r-grun@users.noreply.github.com>
2025-01-19 23:43:45 +0100

    Update config parameters

---
+++ image_colorization/config.py
@@ -0,0 +1,14 @@
+# paths for training
+TRAIN_ANAGLYPH_FILE = "../../test_data/train_anaglyphs.txt"
+TRAIN_REVERSED_FILE = "../../test_data/train_reversed.txt"
+
+# paths for validation
+VALIDATION_ANAGLYPH_FILE = "../../test_data/val_anaglyphs.txt"
+VALIDATION_REVERSED_FILE = "../../test_data/val_reversed.txt"
+
+# paths for testing
+TEST_ANAGLYPH_FILE = "../../test_data/test_anaglyphs.txt"
+TEST_REVERSED_FILE = "../../test_data/test_reversed.txt"
+
+# image size
+IMAGE_SIZE=256
