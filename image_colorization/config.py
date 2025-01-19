127b893fec0cc19387fc179b18bd13cd2e4f9e0b
Robin <44874951+r-grun@users.noreply.github.com>
2025-01-19 23:55:38 +0100

    Add notebook for testing model

--- image_colorization/config.py
+++ image_colorization/config.py
@@ -1,14 +1,20 @@
+# DATA
 # paths for training
-TRAIN_ANAGLYPH_FILE = "../../test_data/train_anaglyphs.txt"
-TRAIN_REVERSED_FILE = "../../test_data/train_reversed.txt"
+TRAIN_ANAGLYPH_FILE = "../../test_data/train_anaglyphs.txt" # path to the file containing the paths to the anaglyph images
+TRAIN_REVERSED_FILE = "../../test_data/train_reversed.txt" # path to the file containing the paths to the reversed anaglyph images
 
 # paths for validation
-VALIDATION_ANAGLYPH_FILE = "../../test_data/val_anaglyphs.txt"
-VALIDATION_REVERSED_FILE = "../../test_data/val_reversed.txt"
+VALIDATION_ANAGLYPH_FILE = "../../test_data/val_anaglyphs.txt" # path to the file containing the paths to the anaglyph images
+VALIDATION_REVERSED_FILE = "../../test_data/val_reversed.txt" # path to the file containing the paths to the reversed anaglyph images
 
 # paths for testing
-TEST_ANAGLYPH_FILE = "../../test_data/test_anaglyphs.txt"
-TEST_REVERSED_FILE = "../../test_data/test_reversed.txt"
+TEST_ANAGLYPH_FILE = "../../test_data/test_anaglyphs.txt" # path to the file containing the paths to the anaglyph images
+TEST_REVERSED_FILE = "../../test_data/test_reversed.txt" # path to the file containing the paths to the reversed anaglyph images
 
 # image size
-IMAGE_SIZE=256
+IMAGE_SIZE=256 # size of the image (width = height)
+
+#########################################################################
+
+# TRAINING PARAMETERS
+EPOCHS = 20 # number of training epochs
